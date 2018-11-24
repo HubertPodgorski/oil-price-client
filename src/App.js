33631f4ec0fb6ecd6a89d8674ae48a5971b0a83a
Hubert Podgórski <podhub.ex@boozt.com>
2018-11-24 18:59:23 +0100

    addStation

--- src/App.js
+++ src/App.js
@@ -1,36 +1,21 @@
 import React, { Component } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import { httpRequest } from './helpers/httpRequest';
+import AddStation from './containers/AddStation';
 
 class App extends Component {
-  componentDidMount = () => {
-    const url = "http://10.250.162.190:3000/oil-stations";
-    const method = "GET";
+    componentDidMount = () => {
+        const url = "http://10.250.162.190:3000/oil-stations";
+        const method = "GET";
 
-    httpRequest(url, method).then(response => console.log(response));
-};
+        httpRequest(url, method).then(response => console.log(response));
+    };
 
-  render() {
-    return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <AddStation />
+        );
+    }
 }
 
 export default App;
---
+++ src/containers/AddStation.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class AddStation extends Component {
+    render() {
+        return (
+            <section>
+                I am AddStation container
+            </section>
+        );
+    }
+}
+
+export default AddStation;
